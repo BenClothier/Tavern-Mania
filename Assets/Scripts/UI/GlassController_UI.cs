84378084aeec0c55c08610af2dd1789ac2cfc5de
Ben Clothier <ben.clothier8@icloud.com>
2022-11-10 13:11:29 +0000

    Add animation to glass filling

--- Assets/Scripts/UI/GlassController_UI.cs
+++ Assets/Scripts/UI/GlassController_UI.cs
@@ -7,6 +7,10 @@ public class GlassController_UI : MonoBehaviour
 {
     [SerializeField] private DrinkHeld characterDrink;
     [SerializeField] private Image glassFillImage;
+    [SerializeField] private float animationSpeed;
+
+    private float targetFillAmount;
+    private Color targetColour;
 
     private void Awake()
     {
@@ -19,7 +23,19 @@ public class GlassController_UI : MonoBehaviour
     {
         Debug.Log("Updating glass UI.");
 
-        glassFillImage.fillAmount = (float)liquids.Count / 3;
-        glassFillImage.color = liquids.Aggregate(Color.black, (col, liq) => col + liq.colour) / liquids.Count;
+        targetFillAmount = (float)liquids.Count / 3;
+        targetColour = liquids.Aggregate(Color.black, (col, liq) => col + liq.colour) / liquids.Count;
+
+        // If first liquid to be added, just take it's colour
+        if (liquids.Count == 1)
+        {
+            glassFillImage.color = liquids[0].colour;
+        }
+    }
+
+    private void Update()
+    {
+        glassFillImage.fillAmount = Mathf.Lerp(glassFillImage.fillAmount, targetFillAmount, animationSpeed * Time.deltaTime);
+        glassFillImage.color = Color.Lerp(glassFillImage.color, targetColour, animationSpeed * Time.deltaTime);
     }
 }
