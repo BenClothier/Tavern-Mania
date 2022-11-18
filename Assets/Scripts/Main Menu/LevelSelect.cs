842d28163477297f4dab0d3df8ed70aa2fef98a2
Hal <millarhal@gmail.com>
2022-11-18 14:44:52 +0000

    Add Main menu and Level Select

---
+++ Assets/Scripts/Main Menu/LevelSelect.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    public void PlayLevelSelect()
+    {
+        SceneManager.LoadScene(sceneName: "LevelSelect");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
